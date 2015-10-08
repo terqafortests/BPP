9dd179c65678546390f67c7f88d2067adf9fa01c
terqafortests <ter.qa.for.tests@gmail.com>
2015-10-08 18:00:14 +0300

    Added some locators

--- src/test/java/pages/SelectPaperPage.java
+++ src/test/java/pages/SelectPaperPage.java
@@ -8,6 +8,8 @@ public class SelectPaperPage extends MainClass {
 	
 	public static By open = By.id("open_button");
 	public static By logout = By.id("logout_button");
+	public static String iFrame = "iframe-layout-instance";
+	public static By messageArea = By.cssSelector("#myModal p");
 	
 	public static void selectCourse(String courseName) {
 		clickOn(By.xpath("//div[@id='myModal']//label[contains(., '" + courseName + "')]"));
