e5d734ceb56d9d133ffa7f6357716b0ede6e9ee1
terqafortests <ter.qa.for.tests@gmail.com>
2015-10-08 13:56:10 +0300

    Improved getElement method and added clickOn method

--- src/test/java/utils/MainClass.java
+++ src/test/java/utils/MainClass.java
@@ -3,18 +3,27 @@ package utils;
 import java.util.NoSuchElementException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainClass extends WebBrowser {
 	
 	public static WebElement getElement(By by) {
+		WebDriverWait wait = new WebDriverWait(Driver(), 60);
 		WebElement element = null;
 		try {
-			element = Driver().findElement(by);
+			element = wait.until(ExpectedConditions.visibilityOf(Driver().findElement(by)));
 			return element;
 		} catch (NoSuchElementException e) {
 			System.out.println("Cannot find Element on the page" + e.getStackTrace());
 		}
 		return element;
 	}
+	
+	public static void clickOn(By by) {
+		getElement(by).click();
+	}
+	
+	
 
 }
