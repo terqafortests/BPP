751f38bdd38ecd2c2c1e74a877ffbc931c1181d2
terqafortests <ter.qa.for.tests@gmail.com>
2015-10-09 16:37:27 +0300

    Updated Tests

---
+++ src/test/java/testNGClasses/PORT3863.java
@@ -0,0 +1,48 @@
+package testNGClasses;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+import pages.LoginPage;
+import pages.MyCalendarPage;
+import pages.MyGroupsPage;
+import pages.MyLearningPage;
+import pages.SelectPaperPage;
+import utils.MainClass;
+
+public class PORT3863 extends MainClass {
+  @Test
+  public void port3863() {
+		LoginPage.login("BP5003200", "Gr091714");
+		switchToFrame(SelectPaperPage.iFrame);
+		SelectPaperPage.selectCourse("Roma Test Course 1");
+		clickOn(SelectPaperPage.open);
+		clickOn(MyLearningPage.helpLink);
+		switchToTab(1);
+		Assert.assertEquals(getCurrUrl(), "http://bpp-fusion-test.apolloglobal.int/group/student/help#MyLearning");
+		closeTab(1);
+		
+		sleepFor(1000);
+		
+		switchToTab(0);
+		clickOn(MyCalendarPage.myCalendar);
+		switchToFrame(SelectPaperPage.iFrame);
+		clickOn(MyCalendarPage.helpLink);
+		switchToTab(1);
+		Assert.assertEquals(getCurrUrl(), "http://bpp-fusion-test.apolloglobal.int/group/student/help#MyCalendar");
+		closeTab(1);
+		
+		sleepFor(1000);
+		
+		switchToTab(0);
+		clickOn(MyGroupsPage.myGroups);
+		switchToFrame(SelectPaperPage.iFrame);
+		clickOn(MyGroupsPage.helpLink);
+		switchToTab(1);
+		Assert.assertEquals(getCurrUrl(), "http://bpp-fusion-test.apolloglobal.int/group/student/help#MyGroups");
+		closeTab(1);
+		
+		
+		
+  }
+}
