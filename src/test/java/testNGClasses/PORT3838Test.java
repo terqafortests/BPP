74398e3c33a5343990053e94b4f2f74b0de57c57
terqafortests <ter.qa.for.tests@gmail.com>
2015-10-12 18:23:15 +0300

    Added PORT3838Test

---
+++ src/test/java/testNGClasses/PORT3838Test.java
@@ -0,0 +1,27 @@
+package testNGClasses;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.MyLearningPage;
+import pages.SelectPaperPage;
+import utils.MainClass;
+
+public class PORT3838Test extends MainClass {
+	@Test
+	public void port3838Test() {
+		List<WebElement> topics = null;
+		LoginPage.login("BP5003200", "Gr091714");
+		switchToFrame(SelectPaperPage.iFrame);
+		SelectPaperPage.selectCourse("Roma Test Course 1");
+		clickOn(SelectPaperPage.open);
+		MyLearningPage.selectStep(2);
+		sleepFor(3000);
+		topics = getElements(By.xpath(
+				"//div[@class='wa_courseformat']/div[@class='topics']/div[contains(@class, 'topic') and contains(@class, 'selected')]"));
+		Assert.assertEquals(topics.size(), 4);
+	}
+}
