b744e51bd2ba47020fdd9aacbf0a135ba99b2221
RomaOnyshkiv <Romaonyshkiv@gmail.com>
2015-10-12 07:47:40 -0500

    updated MarkingWorkflow class

--- src/test/java/Actions/MarkingWorkflow.java
+++ src/test/java/Actions/MarkingWorkflow.java
@@ -1,8 +1,11 @@
 package Actions;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import utils.MainClass;
+
+import java.util.List;
 
 /**
  * Created by roma on 10/9/15.
@@ -22,8 +25,12 @@ public class MarkingWorkflow extends MainClass {
         selectStep(8);
         clickOn(By.xpath("//div[@class='buttons']//*[contains(text(), '" + "TAKE ASSESSMENT" + "')]"));
         clickOn(By.linkText("View/grade all submissions"));
-//        Select stud = new Select(getElement(By.linkText("Calendar001 StCal001")));
-        Select stud = new Select(getElement(By.xpath("//div[@class='no-overflow']//[contains(text(), '" + "Calendar001 StCal001" + "')]:: preceding-sibling::*[2]")));
+
+        String name = "Calendar001 StCal001";
+        clickOn(By.xpath("//tr/td[contains(., 'Select " + name + "')]/input[@name='selectedusers']"));
+//        WebElement check = getElement(By.xpath("//tr/td[contains(., '" + name + "')]/preceding-sibling::td[last()]"));
+//        check.click();
+//        clickOn(By.xpath("//tr/td[contains(., '" + name + "')]/preceding-sibling::td[last()]"));
 
         sleepFor(30000);
 
