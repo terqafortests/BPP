d29fbf1f207c5a203e624b8ce44f04dac12aede0
terqafortests <ter.qa.for.tests@gmail.com>
2015-10-15 15:43:48 +0300

    Added new locators to My calendar page

--- src/test/java/pages/MyCalendarPage.java
+++ src/test/java/pages/MyCalendarPage.java
@@ -8,5 +8,13 @@ public class MyCalendarPage extends MainClass {
 	
 	public static By myCalendar = By.xpath("//div[@id='navbarCollapse']//span[contains(text(), 'My Calendar')]");
 	public static By helpLink = By.id("forumhelplink");
+	public static By exportCalendar = By.xpath(".//*[@id='region-main']/div[4]/div/div/div[3]/div[1]/form/div/input[1]");
+	public static By manageSubscription = By.xpath(".//*[@id='region-main']/div[4]/div/div/div[3]/div[2]/form/div/input[1]");
+	public static By preferences = By.xpath(".//*[@id='region-main']/div[2]/form/div/input[1]");
+	public static By newEvent = By.xpath(".//*[@id='region-main']/div[4]/div/div/div[1]/div[1]/form/div/input[4]");
+	public static By detailedViewFor = By.xpath("//label[text()='Detailed month view for:']/following-sibling::select");
+	
+	
+	
 
 }
