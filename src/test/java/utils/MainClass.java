8aba27675b383c3de652ce84728862bad0db26af
lion17s <serhii.sheptytskyi@gmail.com>
2015-12-11 10:51:32 +0200

    Updated MainClass

--- src/test/java/utils/MainClass.java
+++ src/test/java/utils/MainClass.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.asserts.SoftAssert;
-
 import com.relevantcodes.extentreports.LogStatus;
 
 public class MainClass extends WebBrowser {
